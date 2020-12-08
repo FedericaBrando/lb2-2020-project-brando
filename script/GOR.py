58fc2f4a563e41391d60e8a8208b9542ff320b12
FedericaBrando <fecke.smile@hotmail.it>
2020-12-08 11:20:34 +0100

    GOR pred + results draft

--- script/GOR.py
+++ script/GOR.py
@@ -5,12 +5,24 @@
 
 import os
 import numpy as np
-from Utility.Utilis import Pssm, Dssp
-from math import log
+from Utility.Utilis import Pssm, Dssp, Stats
+from math import log2
+from sklearn.model_selection import PredefinedSplit
+from sklearn.metrics import classification_report, accuracy_score
+from statistics import mean
 
 class GOR():
 
-    def __init__(self, window=17):
+    def __init__(self, tr_db_dir, tes_dir, cv = None, tes_id_path = None, tr_id_path = None, window=17, out = None):
+        # init directories
+        self.directory = tr_db_dir
+        self.tr_id_path = tr_id_path
+        self.directorytest = tes_dir
+        self.te_id_path = tes_id_path
+        self.out = out
+        self.cv = cv
+
+        # init model mx
         self.window = window
         self.pad = self.window // 2
         self.E = np.zeros([self.window, 20], dtype=float)
@@ -19,55 +31,85 @@ class GOR():
         self.r = np.zeros([self.window, 20], dtype=float)
         self.SS = {}
 
-    def training(self, database_directory):
+        # init results
+        self.model = self.training().information_mx()
+        self.y_pred =self.predict()
+        self.dict_pred = {}
 
-        self.directory = database_directory
+    @staticmethod
+    def get_pssmids(dir):
 
-        id_list = []
+        IDsPssm = []
+        k = 0
 
-        for filename in sorted(os.listdir(self.directory)):
+        for filename in sorted(os.listdir(dir)):
             if filename.endswith('.pssm'):
-                if filename[:-5] not in id_list:
-                    id_list.append(filename[:-5])
+                k += 1
+                if Pssm.check_empty(Pssm(filename[:-5],dir).parse()):
+                    IDsPssm.append(filename[:-5])
 
-                    # parsa il file pssm ed estrae il profilo
-                    prf = Pssm(filename[:-5], self.directory).parse()
+        return IDsPssm
 
-                    # parsa il file dssp ed estrae la sequenza amminoacidica
-                    ss_seq = Dssp(filename[:-5], self.directory).parse_dssp()
+    def training(self, id_l = None):
 
-                    if Pssm.check_empty(prf):
+        if self.tr_id_path != None and id_l == None:
+            with open(self.tr_id_path, 'r') as f:
+                id_file = f.readlines()
+                id_list = [x.strip() for x in id_file]
 
-                        tmp = np.zeros([self.pad, 20], dtype=float)
-                        prof = np.concatenate((tmp, prf, tmp))
+        elif self.tr_id_path == None and id_l == None:
+            id_list = []
 
-                        ss = 0
-                        p = 0
-                        q = self.window
+            for filename in sorted(os.listdir(self.directory)):
+                if filename.endswith('.pssm'):
+                    if filename[:-5] not in id_list:
+                        id_list.append(filename[:-5])
 
-                        while q != len(prof)+1 and ss != len(ss_seq)+1 :
+        else:
+            id_list_dir = self.get_pssmids(self.directory)
+            id_list = list(set(id_l).intersection(id_list_dir))
 
-                            if ss_seq[ss] == '-' :
-                                self.C += prof[p :q]
-                                self.r += prof[p :q]
-                                self.SS[ss_seq[ss]] = self.SS.get(ss_seq[ss], 0) + 1
-                                ss += 1
-                                p += 1
-                                q += 1
-                            elif ss_seq[ss] == 'H' :
-                                self.H += prof[p :q]
-                                self.r += prof[p :q]
-                                self.SS[ss_seq[ss]] = self.SS.get(ss_seq[ss], 0) + 1
-                                ss += 1
-                                p += 1
-                                q += 1
-                            elif ss_seq[ss] == 'E' :
-                                self.E += prof[p :q]
-                                self.r += prof[p :q]
-                                self.SS[ss_seq[ss]] = self.SS.get(ss_seq[ss], 0) + 1
-                                ss += 1
-                                p += 1
-                                q += 1
+
+        for id in id_list:
+
+            # parsa il file pssm ed estrae il profilo
+            prf = Pssm(id, self.directory).parse()
+
+            # parsa il file dssp ed estrae la sequenza amminoacidica
+            ss_seq = Dssp(id, self.directory).parse_dssp()
+
+            if Pssm.check_empty(prf):
+
+                tmp = np.zeros([self.pad, 20], dtype=float)
+                prof = np.concatenate((tmp, prf, tmp))
+
+                ss = 0
+                p = 0
+                q = self.window
+
+                while q != len(prof)+1 and ss != len(ss_seq)+1 :
+
+                    if ss_seq[ss] == '-' :
+                        self.C += prof[p :q]
+                        self.r += prof[p :q]
+                        self.SS[ss_seq[ss]] = self.SS.get(ss_seq[ss], 0) + 1
+                        ss += 1
+                        p += 1
+                        q += 1
+                    elif ss_seq[ss] == 'H' :
+                        self.H += prof[p :q]
+                        self.r += prof[p :q]
+                        self.SS[ss_seq[ss]] = self.SS.get(ss_seq[ss], 0) + 1
+                        ss += 1
+                        p += 1
+                        q += 1
+                    elif ss_seq[ss] == 'E' :
+                        self.E += prof[p :q]
+                        self.r += prof[p :q]
+                        self.SS[ss_seq[ss]] = self.SS.get(ss_seq[ss], 0) + 1
+                        ss += 1
+                        p += 1
+                        q += 1
 
         return self
 
@@ -82,107 +124,182 @@ class GOR():
             self.r[row] = self.r[row] / n
 
         tot = sum(self.SS.values())
-
         for key in self.SS :
             self.SS[key] /= tot
 
-
         return self
 
-    def information_mx(self, out=False):
+    def information_mx(self):
+
         self.norm()
 
         self.infE = np.zeros([self.window, 20], dtype=float)
         self.infH = np.zeros([self.window, 20], dtype=float)
         self.infC = np.zeros([self.window, 20], dtype=float)
 
+
         for i in range(self.window) :
             for k in range(20):
-                self.infE[i][k] = log(self.E[i][k] / (self.SS['E'] * self.r[i][k]), 2)
-                self.infC[i][k] = log(self.C[i][k] / (self.SS['-'] * self.r[i][k]), 2)
-                self.infH[i][k] = log(self.H[i][k] / (self.SS['H'] * self.r[i][k]), 2)
+                self.infE[i][k] = log2(self.E[i][k] / (float(self.SS['E']) * self.r[i][k]))
+                self.infC[i][k] = log2(self.C[i][k] / (float(self.SS['-']) * self.r[i][k]))
+                self.infH[i][k] = log2(self.H[i][k] / (float(self.SS['H']) * self.r[i][k]))
 
-        if out:
+        if self.out != None:
             self.Inf = np.concatenate((self.infH, self.infE, self.infC))
-            np.savetxt(self.directory + '/Inf_mx' + ".tsv", self.Inf, delimiter="\t")
+            np.savetxt(self.out + '/Inf_mx' + ".tsv", self.Inf, delimiter="\t")
         return self
 
-    def predict(self, database_dir, out=False):
+    def predict(self, id_lis = None):
 
-        dir = database_dir
+        dir = self.directorytest
         dict_out = {}
-        l = '-EH'
-        id_list = []
+        l = '210'
 
-        for filename in sorted(os.listdir(dir)):
-            if filename.endswith('.pssm'):
-                if filename[:-5] not in id_list:
-                    id_list.append(filename[:-5])
-                    seq_name = '>' + filename[:-5]
+        if self.te_id_path != None and id_lis == None:
+            with open(ids_path_file, 'r') as f :
+                id_file = f.readlines()
+                id_l = [x.strip() for x in id_file]
 
-                    # parsa il file pssm ed estrae il profilo
-                    prf = Pssm(filename[:-5], dir).parse()
+        elif self.te_id_path == None and id_lis == None:
+            id_l = []
 
-                    if Pssm.check_empty(prf):
+            for filename in sorted(os.listdir(dir)) :
+                if filename.endswith('.pssm') :
+                    if filename[:-5] not in id_l:
+                        id_l.append(filename[:-5])
 
-                        tmp = np.zeros([self.pad, 20], dtype=float)
-                        prof = np.concatenate((tmp, prf, tmp))
+        else:
+            id_list_dir = self.get_pssmids(self.directory)
+            id_l = list(set(id_lis).intersection(id_list_dir))
 
-                        p = 0
-                        r = self.window
-                        seq_pred = ''
+        sseq = ''
+        for ID in id_l:
+            seq_name = '>' + ID
 
-                        while r != len(prof) + 1:
+            # parsa il file pssm ed estrae il profilo
+            prf = Pssm(ID, dir).parse()
 
-                            p_C = np.sum(prof[p:r]*self.infC)
-                            p_H = np.sum(prof[p:r]*self.infH)
-                            p_E = np.sum(prof[p:r]*self.infE)
-                            sspred = [p_C, p_E, p_H]
-                            seq_pred = seq_pred + l[sspred.index(max(sspred))]
+            if Pssm.check_empty(prf):
 
-                            p, r = p + 1, r + 1
+                tmp = np.zeros([self.pad, 20], dtype=float)
+                prof = np.concatenate((tmp, prf, tmp))
 
-                        dict_out[seq_name] = seq_pred
+                p = 0
+                r = self.window
+                seq_pred = ''
 
+                while r != len(prof) + 1:
 
-        if out:
+                    p_C = np.sum(prof[p:r]*self.infC)
+                    p_H = np.sum(prof[p:r]*self.infH)
+                    p_E = np.sum(prof[p:r]*self.infE)
+                    sspred = [p_C, p_E, p_H]
+                    seq_pred = seq_pred + l[sspred.index(max(sspred))]
+
+                    p, r = p + 1, r + 1
+
+                dict_out[seq_name] = seq_pred
+                sseq += seq_pred
+
+        if self.out != None:
             i = 0
-            with open(dir + '/predicted_ss.fasta', 'w') as pred_out:
+            with open(self.out + '/predicted_ss.fasta', 'w') as pred_out:
                 for k in sorted(dict_out.keys()):
                     i += 1
                     pred_out.write(k + '\n' + dict_out[k]+'\n')
             print(i)
 
+        self.y_pred = list(map(int,sseq))
+        self.dict_pred = dict_out
 
-        return dict_out
+        return self
+
+    @staticmethod
+    def getytrue(dir,name):
+
+        seq = Dssp(name, dir).parse_dssp()
+        o = seq.translate(str.maketrans({'-' : '2', 'E' : '1', 'H' : '0'}))
+        y_true = list(list(map(int, o)))
+        return y_true
+
+    def clsreport(self):
+        ss = ['E', 'C', 'H']
+        y_pred = self.y_pred
+        y_true = []
+        for key in self.dict_pred:
+            y_true += self.getytrue(self.directorytest, key[1:])
+
+
+        mcc = Stats.MCC(y_true, y_pred, True)
+        # acc = Stats.ACC(y_true, y_pred, True)
+        acc2 = accuracy_score(y_true, y_pred)
+
+        report = classification_report(y_true, y_pred,
+                                       target_names= ['H', 'E', 'C'],
+                                       output_dict=True)
+        report['ACC'] = acc2
+
+        for key in mcc:
+            report[key]['MCC'] = mcc[key]
+
+        print('ACC: {}'.format(report['ACC']))
+        for key in ss :
+            print('{0}\tprecision\t{1:0.5f}\trecall\t{2:0.5f}\tMCC\t{3:0.5f}'.format(key,
+                                                                                     report[key]['precision'],
+                                                                                     report[key]['recall'],
+                                                                                     report[key]['MCC']))
+
+
+        return report
+
+    def crossval(self):
+        if self.cv != None:
+            self.directorytest = directory_train
+            cv = []
+            ts = []
+            c = 0
+            for file in sorted(os.listdir(dir)) :
+                if file.__contains__('set'):
+                    with open(dir+file, 'r') as f:
+                        lines = f.readlines()
+                        Set = [x.strip() for x in lines]
+                        ts += [c for i in range(len(Set))]
+                        cv += Set
+                        c += 1
+
+            cv = np.array(cv)
+            ps = PredefinedSplit(test_fold=ts)
+            fold = 1
+            for train_index, test_index in ps.split():
+                self.model = self.training(list(cv[train_index])).information_mx()
+                self.pred = self.predict(list(cv[test_index]))
+
+                print('FOLD{}:'.format(fold))
+                self.report = self.model.clsreport()
+                fold += 1
+
+            return self
 
 
 if __name__ == '__main__':
+
     import timeit
-    import argparse
+    import os
 
     start = timeit.default_timer()
 
-    parser = argparse.ArgumentParser(description='Predicting SS with GOR method')
+    directory_train = '../training_file'
+    directory_test = '../blindset/test_file'
 
-    parser.add_argument('--dir_db_train', '-dirTr', type=str,
-                        help='directory with .pssm and .dssp file of training set')
-    parser.add_argument('--dir_db_test', '-dirTe', type=str,
-                        help='directory with .pssm and .dssp file of test set')
-    parser.add_argument('--window', '-win', type=int, help='size of the windows (int)')
+    dir = '../cv/'
 
-    args = parser.parse_args()
+    model = GOR(directory_train, directory_test, cv = dir, window=17)
 
-    dirTr, w, dirTe = args.dir_db_train, args.window, args.dir_db_test
+    model.crossval()
 
-    model = GOR(window=w).training(dirTr).information_mx(out=True)
-
-    model.predict(dirTe, True)
-
-    # directory = '../training_file'
-    # model = GOR(window=17).training(directory).information_mx(out=True)
-    # output = model.predict('../blindset/psiblast_output', True)
+    model.predict().clsreport()
 
     stop = timeit.default_timer()
 
     print('Time: ', stop - start)
+
--- script/Utility/Utilis.py
+++ script/Utility/Utilis.py
@@ -3,7 +3,10 @@
 """ This is where the manual goes
     """
 import numpy as np
+from statistics import mean
+from sklearn.metrics import matthews_corrcoef, accuracy_score, classification_report
 
+import math
 
 class Pssm:
 
@@ -33,9 +36,19 @@ class Pssm:
             for line in pssm[3 :-6] :
                 l = line.split()
                 mx.append(Pssm.normalize(l[22:-2]))
-            np_prof = np.vstack(mx)
+            self.np_prof = np.vstack(mx)
 
-        return np_prof
+        return self.np_prof
+
+    def svm_parse(self):
+
+        prf_list = []
+
+
+        for row in self.np_prof:
+            for col in self.np_prof:
+                prf_list.append(self.np_prof[row][col])
+
 
 
 
@@ -50,3 +63,53 @@ class Dssp():
         with open(self.path) as fdssp:
             fdssp.readline()
             return fdssp.readline().rstrip()
+
+
+class Stats():
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def MCC(y_true, y_pred, single = False):
+        sslist = [0, 1, 2]
+        ss = 'HEC'
+
+        mcc = {}
+        for ssclass in sslist :
+            yp_tmp = [ss if ss == ssclass else 9 for ss in y_pred]
+            yt_tmp = [ss if ss == ssclass else 9 for ss in y_true]
+            mcc_tmp = matthews_corrcoef(yp_tmp, yt_tmp)
+            mcc[ss[ssclass]] = mcc_tmp
+
+        meanMCC = mean(mcc.values())
+
+        if single:
+            return mcc
+
+        else: return meanMCC
+
+    @staticmethod
+    def ACC(y_true, y_pred, single = False):
+        sslist = [0, 1, 2]
+
+        acc = []
+        for ssclass in sslist :
+            yp_tmp = [ss if ss == ssclass else 9 for ss in y_pred]
+            yt_tmp = [ss if ss == ssclass else 9 for ss in y_true]
+            acc_tmp = accuracy_score(yp_tmp, yt_tmp)
+            acc.append(acc_tmp)
+        meanACC = mean(acc)
+
+        if single :
+            return acc
+
+        else :
+            return meanACC
+
+if __name__ == '__main__':
+
+    yt = np.array([0,0,2,1,2,0,1,0,1,1,1,1])
+    yp = np.array([0,0,0,1,2,0,1,0,1,1,1,1])
+
+    Stats.clsreport(yt,yp)
